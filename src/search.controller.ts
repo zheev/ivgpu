7a7f695a285edaf531a1b9edabec3444aa946d37
zheev-nb <zhebrikov@zheev.ru>
2021-05-28 22:52:02 +0300

    add controller

---
+++ src/search.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Get, Query } from '@nestjs/common';
+
+@Controller('search')
+export class SearchController{
+
+    @Get()
+    get(@Query('name') name): string {
+        console.log(name);
+        return 'OK';
+    }
+}
